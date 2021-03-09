a45215b6a181228d027fcc1db0ca63ea43011c63
Harrison Noh <nohharri@github.com>
2021-03-09 07:37:35 -0500

    Added basic properties to make work

--- express/server.js
+++ express/server.js
@@ -1,3 +1,8 @@
+/**
+ * Main entry point for the app. This should only be used
+ * for routing.
+ */
+
 'use strict';
 const express = require('express');
 const path = require('path');
@@ -6,11 +11,13 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
+
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  res.json({
+    'hello': 'hi!'
+  });
 });
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
