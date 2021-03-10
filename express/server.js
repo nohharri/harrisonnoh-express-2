8a08d750684eb3b3d6f88518c4a84aaea0b5f05c
Harrison Noh <nohharri@github.com>
2021-03-10 13:27:24 -0500

    test

--- express/server.js
+++ express/server.js
@@ -27,16 +27,23 @@ router.get('/test', (req, res) => res.json({ 'poop': 'change' }));
 
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+// router.post('/graph', (req, res) => graphqlHTTP({ graphiql: true, schema: schema }));
+
+router.get('/poop', (req, res) => ( res.json({ 'test': 'test2' })));
+
+
 app.use(bodyParser.json());
 
-//app.use('/.netlify/functions/server', router);  // path must route to lambda
-//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
-/** GraphQL */
+/** GraphQL. I want to use a path here but I dont know how to make it work. */
 app.use('/graphql', graphqlHTTP({
   graphiql: true,
   schema: schema,
 }));
+
+//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
