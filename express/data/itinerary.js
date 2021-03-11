1a48c997d5266945386c5f72c4addcd07f9b0823
Harrison Noh <nohharri@github.com>
2021-03-10 20:42:01 -0500

    added more to itinerary

--- express/data/itinerary.js
+++ express/data/itinerary.js
@@ -6,8 +6,29 @@ const itineraries = [
         order: 0, 
         description: "Amer will leave his house at 5pm. We try to be on our way by 5:30. Please be prepared to be picked up.",
         map: "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1511572.505984863!2d-86.45915196334781!3d42.272331443496945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x8824c1264437bded%3A0x225564173939d6b5!2s2800%20Homewood%20Drive%2C%20Troy%2C%20MI!3m2!1d42.5955504!2d-83.2043719!4m5!1s0x880e2c3cd0f4cbed%3A0xafe0a6ad09c0c000!2sChicago%2C%20IL!3m2!1d41.8781136!2d-87.6297982!5e0!3m2!1sen!2sus!4v1615417144842!5m2!1sen!2sus",
+    },
+    { 
+        id: 1,
+        tripId: 0,
+        order: 1, 
+        description: "Thirty minutes before arriving, we should buy pizza/drinks. If we don't arrive on time, we will have to look around for anything that is still open. Arrive at Hotel Audrey 🏨. Check-out is the next day at noon 🌅. We have two rooms.",
+        map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.",
+    },
+    { 
+        id: 2,
+        tripId: 0,
+        order: 2, 
+        description: "We will be grabbing hotpot at 12pm Saturday. Hotpot is a popular East Asian cuisine where meats and vegetables are eaten around a hot soup. Vegetarian friendly. I made a reservation for 12pm.",
+        map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25277.77317799594!2d-87.6540153673747!3d41.84849450109007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e2c646b6873d9%3A0x8c900e8c0e004931!2zSGFwcHkgTGFtYiBIb3QgUG90LCBDaGljYWdvIOW_q-S5kOWwj-e-ig!5e0!3m2!1sen!2sus!4v1615425702336!5m2!1sen!2sus",
+    },
+    { 
+        id: 3,
+        tripId: 0,
+        order: 3, 
+        description: "We will grab tacos or dinner somewhere. We don't need a reservation so it's not too important what we decide to do.",
     }
 ];
+
 
 module.exports = itineraries;
 
